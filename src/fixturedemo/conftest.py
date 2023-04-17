e829537b58932e8415716588c6ed1d6eeea756ca
bisma.latif <bisma.latif@10pearls.com>
2023-04-18 00:51:41 +0500

    Initial commit

---
+++ src/fixturedemo/conftest.py
@@ -0,0 +1,37 @@
+import time
+import pytest
+from selenium import webdriver
+
+
+# browser = "chrome"
+# from src.fixturedemo import settings
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    # parser.addoption("--browser", action="store", default=settings.browser)
+    # parser.addoption("--env", action="store", default=settings.env)
+
+
+@pytest.fixture
+def getBrowser(request):
+    _browser = request.config.getoption("--browser")
+    return _browser
+
+
+@pytest.fixture
+def getDriver(request, getBrowser):
+    _driver = None
+    print("browser from getBrowser method - " + getBrowser)
+    if getBrowser == "chrome":
+        _driver = webdriver.Chrome("drivers/chromedriver/chromedriver.exe")
+    elif getBrowser == "firefox":
+        _driver = webdriver.Firefox("drivers/geckodriver/geckodriver.exe")
+    # env = request.config.getoption("--env")
+    # _driver.get("https://www." + env + ".saucedemo.com/index.html")
+    _driver.get("https://google.com")
+    _driver.implicitly_wait(20)
+    request.cls.driver = _driver
+    yield request.cls.driver
+    time.sleep(2)
+    request.cls.driver.quit()
